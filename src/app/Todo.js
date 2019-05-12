08f2c81da23aa47e65aaee61fac446e17246adab
wali89 <wali.rahman89@yahoo.com>
2019-05-12 18:50:12 -0400

    added state

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nums: [1, 2, 3]
+    }
+  }
   render() {
     return (
-      <div>hello</div>
+      <div>hello
+
+      <button>click </button>
+      </div>
     );
   }
 }
